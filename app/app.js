6d9fa34905165d391cdfaafadfb4bc9e3226f9eb
Mykola Voronin <mvoronin@Mykolas-MacBook-Pro.local>
2017-08-09 13:16:39 +0300

    Refactored ChatApp component

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
+'use strict';
+
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import { configureStore } from './store'
 
-import ChatApp from './chatApp';
+import ChatApp from './components/ChatApp';
 
 const store = configureStore();
 
