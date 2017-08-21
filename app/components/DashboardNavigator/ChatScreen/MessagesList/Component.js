f650043a3efdb8ba7e79191bed74984a71a43868
Evgeniy Antonov <antonoff.evgeniy@gmail.com>
2017-08-21 20:00:53 +0300

    Added placeholder

--- app/components/DashboardNavigator/ChatScreen/MessagesList/Component.js
+++ app/components/DashboardNavigator/ChatScreen/MessagesList/Component.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { ListView, List, FlatList } from 'react-native';
+import { List, FlatList, Text } from 'react-native';
 
 import styles from './Styles';
 
 import MessageRow from './MessageRow';
 import { getChatItems } from '../../../../store/chat/selectors';
+import translations from '../../../../i18n';
 
 const ITEM_HEIGHT = 50;
 
@@ -18,12 +19,23 @@ class MessageListComponent extends Component {
         }
     }
 
+    emptyList() {
+        return (
+            <Text
+                style={styles.placeholder}>
+                {translations.t('placeholder')}
+            </Text>
+        );
+      }
+
     render() {
         const data = getChatItems(this.props.data).reverse();
+        const contentContainerStyle = data.length ? null : styles.flatlistContainerStyle;
         return (
             <FlatList
                 ref='flatList'
                 style={styles.container}
+                contentContainerStyle={contentContainerStyle}
                 data={data}
                 keyExtractor={item => item.time}
                 renderItem={({ item }) => { 
@@ -33,6 +45,7 @@ class MessageListComponent extends Component {
                     {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
                 )}
                 inverted
+                ListEmptyComponent={this.emptyList.bind(this)}
             />
         );
     }
