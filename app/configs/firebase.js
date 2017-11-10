51fdb97cf3c40411d97d8d513569c4a3c93976b8
Mykola Voronin <nvoronin@rubygarage.org>
2017-11-10 11:35:35 +0200

    Added Firebase config with placeholders

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  projectId: "<PROJECT_ID>",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+}
